e876bd89766449429c595c3c65133ac47a38559a
Milos Vujasinovic <18309829+mvujas@users.noreply.github.com>
2020-10-03 22:48:17 +0200

    load_data now returns column name to index mapping as an element of result tuple

--- data_io.py
+++ data_io.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def load_data(file_path):
     """Loads data from the file under the given file path and returns it in form
-        of a tuple (ids, labels, features) where labels are mapped to numerical value
+        of a tuple (column_name_to_index_mapping, ids, labels, features) 
+        where labels are mapped to numerical value
 
     Parameters
     ----------
@@ -10,7 +11,10 @@ def load_data(file_path):
         Path to the file which data should be read from
     """
     with open(file_path, 'r') as f:
-        num_cols = len(f.readline().split(','))
+        # creating column name to index mapping and calculating number of columns
+        column_names = f.readline().split(',')
+        column_index = {val : ind for ind, val in enumerate(column_names)}
+        num_cols = len(column_names)
         # starts reading the file from beginning
         f.seek(0)
         labels = np.genfromtxt(f, delimiter=",", 
@@ -21,7 +25,7 @@ def load_data(file_path):
         data_columns = np.genfromtxt(f, delimiter=",", 
             skip_header=1, usecols=[0, *range(2, num_cols)])
     ids = data_columns[:, 0]
-    # while features start from column 2 in the file, there are no prediction column
+    # while features start from column 2 in the file, there is no prediction column
     #   in data_columns array and therefore they start from index 1 in the given array 
     features = data_columns[:, 1:]
-    return ids, labels, features
+    return column_index, ids, labels, features
