40b02d9a4aef42538888868059dcce6f8f83dbd9
Milos Vujasinovic <18309829+mvujas@users.noreply.github.com>
2020-10-03 14:56:42 +0200

    Losses moved in implements.py as specified in the description of the project, least_squares_SGD implemented

--- implementations.py
+++ implementations.py
@@ -1,5 +1,84 @@
 import numpy as np
-from losses import MeanSquaredError
+
+class MeanSquaredError:
+    """Class that implements static methods that calculate
+    value and gradient of mean squared error
+    """
+    @staticmethod
+    def calculate(y, tx, w):
+        """Calculates value of mean squared error
+
+        Parameters
+        ----------
+        y : np.ndarray
+            Labels
+        tx : np.ndarray
+            Features
+        w : np.ndarray
+            Parameters of the model
+        """
+        e = y - tx @ w
+        return .5 * np.mean(e ** 2)
+
+    @staticmethod
+    def gradient(y, tx, w):
+        """Calculates gradient of mean squared error
+
+        Parameters
+        ----------
+        y : np.ndarray
+            Labels
+        tx : np.ndarray
+            Features
+        w : np.ndarray
+            Parameters of the model
+        """
+        e = y - tx @ w
+        N = y.shape[0]
+        return -1 / N * (tx.T @ e)
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+
+    Parameters
+    ----------
+    y : np.ndarray
+          Labels
+    tx : np.ndarray
+          Features
+    batch_size : int
+          Number of datapoints in each batch
+    num_batches : int
+          Positive integer indicating a number of batches the data to be split into 
+          [effective value: min(num_batches, math.ceil(len(y) / batch_size))]
+    shuffle : bool
+          Should data be randomly shuffled before being split into batches 
+    """
+    data_size = len(y)
+
+    shuffled_y = y
+    shuffled_tx = tx
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index >= end_index:
+            break
+        yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
 
 # TODO : all functions should return (w, loss) pair
 
@@ -48,7 +127,9 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     loss = MeanSquaredError.calculate(y, tx, weights)
     return (weights, loss)
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+# KEEP IN MIND: Optinal arguments might have to be removed 
+#    as they are not in the description of the project 
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma, batch_size=32, num_batches=1):
     """Calculates the least squares solution using stochastic gradient descent.
     Returns tuple (parameters, loss)
 
@@ -64,9 +145,22 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
          Maximum number of iterations
     gamma : float
          Learning rate 
+    batch_size : int
+          Number of datapoints in each batch
+    num_batches : int
+          Number of batches per each iteration of SGD algorithm
     """
-    # TODO : check whether we can add and delete arguments
-    raise NotImplementedError()
+    weights = initial_w
+    for iteration in range(max_iters):
+        # Stochastic Gradient Descent step
+        batches = batch_iter(y, tx, 
+          batch_size=batch_size, num_batches=num_batches)
+        for y_batch, tx_batch in batches:
+            gradient = MeanSquaredError.gradient(y_batch, tx_batch, weights)
+            weights = weights - gamma * gradient
+    # Calculate loss
+    loss = MeanSquaredError.calculate(y, tx, weights)
+    return (weights, loss)
 
 def ridge_regression(y, tx, lambda_):
     """Implements ridge regression.
