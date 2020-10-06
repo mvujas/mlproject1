e2c8168629450ca3a9a81675a7193521fa9d8a69
Milos Vujasinovic <18309829+mvujas@users.noreply.github.com>
2020-10-06 16:30:59 +0200

    Backward attribute selector added

--- attribute_selection.py
+++ attribute_selection.py
@@ -40,3 +40,38 @@ def forward_attribute_selector(y, x, evaluator,
     
     choosen_attributes.sort()
     return choosen_attributes
+
+
+def backward_attribute_selector(y, x, evaluator, 
+        attribute_subset_size, verbose=False):
+    num_instances, num_attributes = x.shape
+    choosen_attributes = list(range(num_attributes))
+    while len(choosen_attributes) > 0 and \
+            len(choosen_attributes) > attribute_subset_size:
+        current_best = {
+            'index' : None,
+            'score' : None
+        }
+        for local_index, attr_index in enumerate(choosen_attributes):
+            choosen_attributes.remove(attr_index)
+            x_subset = x[:, choosen_attributes]
+            score = evaluator(y, x_subset)
+            if current_best['score'] is None or \
+                    score > current_best['score']:
+                current_best = {
+                    'index' : attr_index,
+                    'score' : score
+                }
+            choosen_attributes.insert(local_index, attr_index)
+
+        choosen_attributes.remove(current_best['index'])
+        
+        if verbose:
+            best_score = current_best['score']
+            best_attributes = ', '.join(map(str, choosen_attributes))
+            print(f' --- BACKWARD ATTRIBUTE SELECTION: ' + 
+                f'Best attribute indexes for size {len(choosen_attributes)}: ' +
+                f'{best_attributes} (score {best_score:.6})')
+    
+    choosen_attributes.sort()
+    return choosen_attributes
